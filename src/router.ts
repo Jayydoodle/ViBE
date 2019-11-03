2ec86f4f4cf64691384d513a99dfd5fb3e43cb6d
Shabir Ghulam <sghulam@udel.edu>
2019-11-03 15:50:39 -0500

    small updates

--- src/router.ts
+++ src/router.ts
@@ -1,20 +1,21 @@
 import express from "express";
 import {Controller} from "./controller";
-import {UserController} from "./user.Controller";
+// tslint:disable-next-line:max-line-length
+import {UserController} from "./userController";
 export class ApiRouter {
     private router: express.Router = express.Router();
     private controller: Controller = new Controller();
-    private userController=new UserController();
+    private userController = new UserController();
 
     // Creates the routes for this router and returns a populated router object
     public getRouter(): express.Router {
         this.router.get("/hello", this.controller.getHello);
         this.router.post("/hello", this.controller.postSomething);
-        this.router.get('/allusers',this.userController.allUsers);
-        this.router.get('/user/:user_id',this.userController.userInfo);
-        this.router.post('/createuser',this.userController.createUser);
-        this.router.put('update/:user_id',this.userController.editUserInfo);
-        this.router.delete('/user/:user_id',this.userController.deleteuser);
+        this.router.get("/allusers", this.userController.allUsers);
+        this.router.get("/user/:user_id", this.userController.userInfo);
+        this.router.post("/createuser", this.userController.createUser);
+        this.router.put("update/:user_id", this.userController.editUserInfo);
+        this.router.delete("/user/:user_id", this.userController.deleteuser);
 
         return this.router;
     }
--- src/userController.ts
+++ src/userController.ts
@@ -2,16 +2,16 @@ import express from "express";
 
 export class UserController {
 
-    //function to create event
+    // function to create event
     public createUser(req: express.Request, res: express.Response): void {
-        
-    //creating a connection to the database:
+
+    // creating a connection to the database:
      const MongoClient = require("mongodb").MongoClient;
      const uri = "mongodb+srv://dummy:dummy@cluster0-ldrzf.mongodb.net/test?retryWrites=true&w=majority";
      const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
      client.connect(() => {
     console.log(client.isConnected());
-    client.db("vibe").collection("collection").insert({inUserCreate : true, description: "this is a test for create user."});//inserts into database
+    client.db("vibe").collection("collection").insert({inUserCreate : true, description: "this is a test for create user."}); // inserts into database
 
   // closes connection
     client.close();
@@ -20,15 +20,15 @@ export class UserController {
      res.send(req.body);
     }
 
-    public userInfo(req: express.Request, res: express.Response) : void {
+    public userInfo(req: express.Request, res: express.Response): void {
 
         const MongoClient = require("mongodb").MongoClient;
         const uri = "mongodb+srv://dummy:dummy@cluster0-ldrzf.mongodb.net/test?retryWrites=true&w=majority";
         const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
         client.connect(() => {
        console.log(client.isConnected());
-       client.db("vibe").collection("collection").insert({inUserInfo : true, description: "this is a test for user info."});//inserts into database
-   
+       client.db("vibe").collection("collection").insert({inUserInfo : true, description: "this is a test for user info."}); // inserts into database
+
      // closes connection
        client.close();
        });
@@ -36,14 +36,14 @@ export class UserController {
         res.send(req.body);
 }
 
-public deleteuser(req: express.Request, res: express.Response) : void {
+public deleteuser(req: express.Request, res: express.Response): void {
 
     const MongoClient = require("mongodb").MongoClient;
     const uri = "mongodb+srv://dummy:dummy@cluster0-ldrzf.mongodb.net/test?retryWrites=true&w=majority";
     const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
     client.connect(() => {
    console.log(client.isConnected());
-   client.db("vibe").collection("collection").insert({indelete : true, description: "this is a test for user delete."});//inserts into database
+   client.db("vibe").collection("collection").insert({indelete : true, description: "this is a test for user delete."}); // inserts into database
 
  // closes connection
    client.close();
@@ -52,14 +52,14 @@ public deleteuser(req: express.Request, res: express.Response) : void {
     res.send(req.body);
 }
 
-public editUserInfo(req: express.Request, res: express.Response) : void {
+public editUserInfo(req: express.Request, res: express.Response): void {
 
     const MongoClient = require("mongodb").MongoClient;
     const uri = "mongodb+srv://dummy:dummy@cluster0-ldrzf.mongodb.net/test?retryWrites=true&w=majority";
     const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
     client.connect(() => {
    console.log(client.isConnected());
-   client.db("vibe").collection("collection").insert({inEditUserInfo : true, description: "this is a test for user Editing info."});//inserts into database
+   client.db("vibe").collection("collection").insert({inEditUserInfo : true, description: "this is a test for user Editing info."}); // inserts into database
 
  // closes connection
    client.close();
@@ -68,14 +68,14 @@ public editUserInfo(req: express.Request, res: express.Response) : void {
     res.send(req.body);
 }
 
-public allUsers(req: express.Request, res: express.Response) : void {
+public allUsers(req: express.Request, res: express.Response): void {
 
     const MongoClient = require("mongodb").MongoClient;
     const uri = "mongodb+srv://dummy:dummy@cluster0-ldrzf.mongodb.net/test?retryWrites=true&w=majority";
     const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
     client.connect(() => {
    console.log(client.isConnected());
-   client.db("vibe").collection("collection").insert({Allusers : true, description: "this is a test for all user info."});//inserts into database
+   client.db("vibe").collection("collection").insert({Allusers : true, description: "this is a test for all user info."}); // inserts into database
 
  // closes connection
    client.close();
