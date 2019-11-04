1de6105399f40768ac8ad66b06d3d05843f960ef
Shabir Ghulam <sghulam@udel.edu>
2019-11-03 19:44:52 -0500

    dhdhasdhas

---
+++ src/event.controller.ts
@@ -0,0 +1,72 @@
+import express from "express";
+import {VibeDatabase} from "./vibe.database";
+
+export class EventController {
+
+    private static database: VibeDatabase = new VibeDatabase();
+
+    // function to create event
+    public createEvent(req: express.Request, res: express.Response): void {
+
+      EventController.database.connect(() => {
+         EventController.database.getClient().db("vibe").collection("collection").insert(
+              req.body
+              ); // inserts into database
+         });
+      // // closes connection
+      res.send(req.body);
+    }
+
+    public eventInfo(req: express.Request, res: express.Response): void {
+
+        EventController.database.connect(() => {
+           EventController.database.getClient().db("vibe").collection("collection").insert(
+                req.body
+                ); // inserts into database
+           });
+        // // closes connection
+        res.send(req.body);
+    }
+
+    public updateEvent(req: express.Request, res: express.Response): void {
+
+        EventController.database.connect(() => {
+           EventController.database.getClient().db("vibe").collection("collection").insert(
+                req.body
+                ); // inserts into database
+           });
+        // // closes connection
+        res.send(req.body);
+    }
+
+public deleteEvent(req: express.Request, res: express.Response): void {
+    EventController.database.connect(() => {
+        EventController.database.getClient().db("vibe").collection("collection").insert(
+            req.body
+            ); // inserts into database
+       });
+    // // closes connection
+    res.send(req.body);
+}
+
+public getEventbyZip(req: express.Request, res: express.Response): void {
+
+    EventController.database.connect(() => {
+        EventController.database.getClient().db("vibe").collection("collection").insert(
+            req.body
+            ); // inserts into database
+       });
+    // // closes connection
+    res.send(req.body);
+}
+
+public getEventsbyState(req: express.Request, res: express.Response): void {
+    EventController.database.connect(() => {
+        EventController.database.getClient().db("vibe").collection("collection").insert(
+            req.body
+            ); // inserts into database
+       });
+    // // closes connection
+    res.send(req.body);
+}
+}
