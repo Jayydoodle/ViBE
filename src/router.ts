23a1597651d3dda0d300d773bc9c7409149b6278
amaad <amaadrafi@gmail.com>
2019-11-03 19:53:55 -0500

    they call me Mr. Clean

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import {Controller} from "./controller";
-import {EventController} from './event.controller'
+import {EventController} from "./event.controller";
 
 // tslint:disable-next-line:max-line-length
 import {UserController} from "./userController";
 
 export class ApiRouter {
     private router: express.Router = express.Router();
-    //private controller: Controller = new Controller();
+    // private controller: Controller = new Controller();
     private userController: UserController = new UserController();
     private eventController: EventController = new EventController();
 
@@ -19,12 +19,11 @@ export class ApiRouter {
         this.router.post("/user", this.userController.createUser);
         this.router.put("user/:user_id/update", this.userController.editUserInfo);
         this.router.delete("/user/:user_id/delete", this.userController.deleteuser);
-        this.router.post('/event',this.eventController.createEvent);
-        this.router.put('/event/:event_id',this.eventController.updateEvent);
-        this.router.delete('/event/:event_id',this.eventController.deleteEvent);
-        this.router.get('/event/zip',this.eventController.getEventbyZip);
-        this.router.get('/event/state',this.eventController.getEventsbyState);
-
+        this.router.post("/event", this.eventController.createEvent);
+        this.router.put("/event/:event_id", this.eventController.updateEvent);
+        this.router.delete("/event/:event_id", this.eventController.deleteEvent);
+        this.router.get("/event/zip", this.eventController.getEventbyZip);
+        this.router.get("/event/state", this.eventController.getEventsbyState);
 
         return this.router;
     }
