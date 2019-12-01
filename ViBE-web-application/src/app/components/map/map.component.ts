e53c10c47b80864960ea214249838d0593f03c21
Jayydoodle <Jayydoodle@gmail.com>
2019-12-01 18:26:11 -0500

    changes

--- ViBE-web-application/src/app/components/map/map.component.ts
+++ ViBE-web-application/src/app/components/map/map.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewChild } from '@angular/core';
 import {} from 'googlemaps';
-import { UserService } from '../services/user.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-map',
