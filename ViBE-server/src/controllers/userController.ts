b710d08db4d896bb400b6bc2e9487c440eb8abc3
Jayydoodle <Jayydoodle@gmail.com>
2019-12-01 02:16:09 -0500

    changes

--- ViBE-server/src/controllers/userController.ts
+++ ViBE-server/src/controllers/userController.ts
@@ -35,60 +35,60 @@ export class UserController {
         res.send(req.body);
     }
 
-public deleteuser(req: express.Request, res: express.Response): void {
+  public deleteuser(req: express.Request, res: express.Response): void {
 
-    const MongoClient = require("mongodb").MongoClient;
-    const uri = "mongodb+srv://dummy:dummy@cluster0-ldrzf.mongodb.net/test?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
-    client.connect(() => {
-    console.log(client.isConnected());
-    client.db("vibe").collection("collection").insert({
-      description: "this is a test for user delete.",
-      indelete : true
+      const MongoClient = require("mongodb").MongoClient;
+      const uri = "mongodb+srv://dummy:dummy@cluster0-ldrzf.mongodb.net/test?retryWrites=true&w=majority";
+      const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
+      client.connect(() => {
+      console.log(client.isConnected());
+      client.db("vibe").collection("collection").insert({
+        description: "this is a test for user delete.",
+        indelete : true
+        }); // inserts into database
+
+  // closes connection
+      client.close();
+    });
+      console.log(client.isConnected());
+      res.send(req.body);
+  }
+
+  public editUserInfo(req: express.Request, res: express.Response): void {
+
+      const MongoClient = require("mongodb").MongoClient;
+      const uri = "mongodb+srv://dummy:dummy@cluster0-ldrzf.mongodb.net/test?retryWrites=true&w=majority";
+      const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
+      client.connect(() => {
+      console.log(client.isConnected());
+      client.db("vibe").collection("collection").insert({
+        description: "this is a test for user Editing info.",
+        inEditUserInfo : true
       }); // inserts into database
 
- // closes connection
-    client.close();
-   });
-    console.log(client.isConnected());
-    res.send(req.body);
-}
+  // closes connection
+      client.close();
+    });
+      console.log(client.isConnected());
+      res.send(req.body);
+  }
 
-public editUserInfo(req: express.Request, res: express.Response): void {
+  public allUsers(req: express.Request, res: express.Response): void {
 
-    const MongoClient = require("mongodb").MongoClient;
-    const uri = "mongodb+srv://dummy:dummy@cluster0-ldrzf.mongodb.net/test?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
-    client.connect(() => {
-    console.log(client.isConnected());
-    client.db("vibe").collection("collection").insert({
-      description: "this is a test for user Editing info.",
-      inEditUserInfo : true
-    }); // inserts into database
+      const MongoClient = require("mongodb").MongoClient;
+      const uri = "mongodb+srv://dummy:dummy@cluster0-ldrzf.mongodb.net/test?retryWrites=true&w=majority";
+      const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
+      client.connect(() => {
+      console.log(client.isConnected());
+      client.db("vibe").collection("collection").insert({
+        Allusers : true,
+        description: "this is a test for all user info."
+      }); // inserts into database
 
- // closes connection
-    client.close();
-   });
-    console.log(client.isConnected());
-    res.send(req.body);
-}
-
-public allUsers(req: express.Request, res: express.Response): void {
-
-    const MongoClient = require("mongodb").MongoClient;
-    const uri = "mongodb+srv://dummy:dummy@cluster0-ldrzf.mongodb.net/test?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
-    client.connect(() => {
-    console.log(client.isConnected());
-    client.db("vibe").collection("collection").insert({
-      Allusers : true,
-      description: "this is a test for all user info."
-    }); // inserts into database
-
- // closes connection
-    client.close();
-   });
-    console.log(client.isConnected());
-    res.send(req.body);
-}
+  // closes connection
+      client.close();
+    });
+      console.log(client.isConnected());
+      res.send(req.body);
+  }
 }
